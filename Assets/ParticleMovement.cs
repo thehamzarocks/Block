c23e8587f4a98b84fc04eda81e0a511b2dab6e1e
thehamzarocks <thehamzarocks@gmail.com>
2018-05-31 01:03:15 +0530

    initial commit

---
+++ Assets/ParticleMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleMovement : MonoBehaviour {
+
+	int impactPosition;
+	float speed;
+	// Use this for initialization
+	void Start () {
+		speed = 2f;
+		impactPosition = 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate(speed*Time.deltaTime,0,0);
+		if (this.transform.position.x >= impactPosition) {
+			Debug.Log ("Impact!");
+			Destroy (this.gameObject);
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.name == "Player") {
+			Debug.Log ("Collided!");
+			Destroy (this.gameObject, 0.3f);
+		}
+	}
+}
