c4ccc268947594198cdf8eb122a053d434b3eedf
thehamzarocks <thehamzarocks@gmail.com>
2018-05-31 14:15:21 +0530

    adding particle lights

--- Assets/ParticleMovement.cs
+++ Assets/ParticleMovement.cs
@@ -4,27 +4,37 @@ using UnityEngine;
 
 public class ParticleMovement : MonoBehaviour {
 
-	int impactPosition;
+	float impactPosition;
 	float speed;
+
 	// Use this for initialization
 	void Start () {
 		speed = 2f;
-		impactPosition = 2;
+		impactPosition = -2.5f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.Translate(speed*Time.deltaTime,0,0);
-		if (this.transform.position.x >= impactPosition) {
-			Debug.Log ("Impact!");
+		this.transform.Translate(0,0,-speed*Time.deltaTime);
+		if (this.transform.position.z <= impactPosition) {			
 			Destroy (this.gameObject);
 		}
 	}
 
 	void OnCollisionEnter(Collision collision) {
-		if (collision.gameObject.name == "Player") {
-			Debug.Log ("Collided!");
-			Destroy (this.gameObject, 0.3f);
+		if (collision.gameObject.name == "Player") {			
+			LightUp ();
+			Destroy (this.gameObject);	
 		}
+	}
+
+	void LightUp() {
+		GameObject lightGameObject = new GameObject("The Light");
+		Light lightComp = lightGameObject.AddComponent<Light>();
+		lightComp.color = Color.yellow;
+		lightComp.intensity = 3;
+		lightComp.areaSize = new Vector2 (7, 7);
+		lightGameObject.transform.position = new Vector3(this.transform.position.x, 5, this.transform.position.z);
+		Destroy (lightGameObject, 0.3f);
 	}
 }
